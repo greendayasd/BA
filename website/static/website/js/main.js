d9d7c72ddc93964826ec1b847f007f3215b8318d
Nicolas <nicolas@brauner.de>
2018-01-02 00:41:28 +0100

    jump and run

--- website/static/website/js/main.js
+++ website/static/website/js/main.js
@@ -1,19 +1,89 @@
+// =============================================================================
+// sprites
+// =============================================================================
+
+//
+// hero sprite
+//
+function Hero(game, x, y) {
+    // call Phaser.Sprite constructor
+    Phaser.Sprite.call(this, game, x, y, 'hero');
+
+    this.anchor.set(0.5, 0.5);
+
+    this.game.physics.enable(this);
+
+    this.body.collideWorldBounds = true;
+}
+
+// inherit from Phaser.Sprite
+Hero.prototype = Object.create(Phaser.Sprite.prototype);
+Hero.prototype.constructor = Hero;
+
+
+Hero.prototype.move = function (direction) {
+    const SPEED = 200;
+    this.body.velocity.x = direction * SPEED;
+};
+
+
+Hero.prototype.jump = function () {
+    const JUMP_SPEED = 600;
+    let canJump = this.body.touching.down;
+
+    if (canJump) {
+        this.body.velocity.y = -JUMP_SPEED;
+    }
+
+    return canJump;
+};
+
+
+// =============================================================================
+// game states
+// =============================================================================
 
 PlayState = {};
 var files = '/static/website/gamefiles/';
 var level = '/static/website/level/';
 
+
+PlayState.init = function () {
+    this.game.renderer.renderSession.roundPixels = true;
+
+    this.keys = this.game.input.keyboard.addKeys({
+        left: Phaser.KeyCode.LEFT,
+        right: Phaser.KeyCode.RIGHT,
+        space: Phaser.KeyCode.SPACEBAR,
+        up: Phaser.KeyCode.UP
+    });
+
+    jump = function () {
+        let didJump = this.hero.jump();
+        if (didJump) {
+            this.sfx.jump.play();
+        }
+    };
+
+    this.keys.up.onDown.add(jump, this);
+
+    this.keys.space.onDown.add(jump, this)
+};
+
 // load game assets here
 PlayState.preload = function () {
     this.game.load.json('level:1', level + 'level01.json');
-    this.game.load.image('background', files + 'background.png');
 
+
+    this.game.load.image('background', files + 'background.png');
     this.game.load.image('ground', files + 'ground.png');
     this.game.load.image('grass:8x1', files + 'grass_8x1.png');
     this.game.load.image('grass:6x1', files + 'grass_6x1.png');
     this.game.load.image('grass:4x1', files + 'grass_4x1.png');
     this.game.load.image('grass:2x1', files + 'grass_2x1.png');
     this.game.load.image('grass:1x1', files + 'grass_1x1.png');
+
+    this.game.load.image('hero', files + 'hero_stopped.png');
 };
 
 // create game entities and set up world here
@@ -22,14 +92,57 @@ PlayState.create = function () {
     this._loadLevel(this.game.cache.getJSON('level:1'));
 };
 
+PlayState.update = function () {
+    this._handleCollisions();
+    this._handleInput();
+};
+
+PlayState._handleCollisions = function () {
+    this.game.physics.arcade.collide(this.hero, this.platforms);
+};
+
+PlayState._handleInput = function () {
+    if (this.keys.left.isDown) { // move hero left
+        this.hero.move(-1);
+    }
+    else if (this.keys.right.isDown) { // move hero right
+        this.hero.move(1);
+    }
+    else { // stop
+        this.hero.move(0);
+    }
+};
+
 PlayState._loadLevel = function (data) {
+    // create all the groups/layers that we need
+    this.platforms = this.game.add.group();
+
     // spawn all platforms
     data.platforms.forEach(this._spawnPlatform, this);
+    // spawn hero and enemies
+    this._spawnCharacters({hero: data.hero, spiders: data.spiders});
+    const GRAVITY = 1200;
+    this.game.physics.arcade.gravity.y = GRAVITY;
 };
 
 PlayState._spawnPlatform = function (platform) {
-    this.game.add.sprite(platform.x, platform.y, platform.image);
+    let sprite = this.platforms.create(
+        platform.x, platform.y, platform.image);
+
+    this.game.physics.enable(sprite);
+    sprite.body.allowGravity = false;
+    sprite.body.immovable = true;
 };
+
+PlayState._spawnCharacters = function (data) {
+    // spawn hero
+    this.hero = new Hero(this.game, data.hero.x, data.hero.y);
+    this.game.add.existing(this.hero);
+};
+
+// =============================================================================
+// entry point
+// =============================================================================
 
 window.onload = function () {
     let game = new Phaser.Game(960, 600, Phaser.AUTO, 'game');
